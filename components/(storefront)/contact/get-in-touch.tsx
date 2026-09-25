c1e98e0c473730aa4b16c710fa8aafedf132b751
Nelson Nachi <devnelsonnachi@gmail.com>
2026-09-25 13:37:15 +0100

    ---

--- components/(storefront)/contact/get-in-touch.tsx
+++ components/(storefront)/contact/get-in-touch.tsx
@@ -462,7 +462,7 @@ export function GetInTouch() {
         {/* Background Image */}
 
         <Image
-          src="/images/banners/contact-banner.jpg"
+          src="/images/banner/thankyouimage.png"
           alt=""
           fill
           sizes="100vw"
